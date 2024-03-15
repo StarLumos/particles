081183279598a750fd69969bb8c859da328713ee
Chelsea Teh <chelsea.ky.teh@gmail.com>
2024-03-14 22:19:56 -0700

    :rocket: `feature` cube wall collision

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,33 @@ const ctx = canvas.getContext("2d")!
 ctx.scale(1, -1)
 ctx.translate(canvas.width / 2, -canvas.height / 2)
 
-var counter = 0
+var xPosition = 0
+var xDirection = -1
+var xSpeed = 5
+var yPosition = 0
+var yDirection = 1
+var ySpeed = 5
 
 function frameloop() {
   ctx.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height)
   
   ctx.fillStyle = 'red'
-  ctx.fillRect(counter, 0, 50, 50)
-  counter -= 1
+  ctx.fillRect(xPosition, yPosition, 50, 50)
+  xPosition += xDirection * xSpeed
+  yPosition += yDirection * ySpeed
+
+  if (xPosition <= -canvas.width/2) {
+    xDirection = 1
+  } else if (xPosition >= canvas.width/2){
+    xDirection = -1
+  }
+
+  if (yPosition <= -canvas.height/2){
+    yDirection = 1
+  } else if (yPosition >= canvas.height/2){
+    yDirection = -1
+  }
+
   requestAnimationFrame(frameloop)
 }
 
